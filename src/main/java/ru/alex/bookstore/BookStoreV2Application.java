9f16a922395504bf5a529d048009e9dbf44dcdec
R0n316 <manyakinsasha@gmail.com>
2024-06-04 21:40:18 +0300

    initial commit

---
+++ src/main/java/ru/alex/bookstore/BookStoreV2Application.java
@@ -0,0 +1,13 @@
+package ru.alex.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreV2Application.class, args);
+    }
+
+}
