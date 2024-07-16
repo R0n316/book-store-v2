ed084ec9207a541a485fb8414250f453ee36f027
R0n316 <manyakinsasha@gmail.com>
2024-07-16 13:55:12 +0300

    add display reviews using swiper js

---
+++ src/main/resources/static/scripts/reviews.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView: 2,
+    freeMode: true,
+    resistance: false
+});
